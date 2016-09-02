2bedf3176f44e8658830fa31a778aca89a6453bb
桑原慶之 <keishikuwabara@31kish.local>
2016-09-03 02:18:09 +0900

    first commit

---
+++ MultiTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiTimer
+//
+//  Created by 桑原慶之 on 2016/09/03.
+//  Copyright © 2016年 KeishiKuwabara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
