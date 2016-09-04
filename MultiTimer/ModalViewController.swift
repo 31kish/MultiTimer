9e4ed885f3ca336368488c79e12fbfdd4659ba81
桑原慶之 <keishikuwabara@31kish.local>
2016-09-05 00:10:59 +0900

    ビューを作成

---
+++ MultiTimer/ModalViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ModalViewController.swift
+//  MultiTimer
+//
+//  Created by 桑原慶之 on 2016/09/04.
+//  Copyright © 2016年 KeishiKuwabara. All rights reserved.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
--- MultiTimer/ViewController.swift
+++ MultiTimer/ViewController.swift
@@ -10,16 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var minuteLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var selectButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        setEvent();
     }
 
+    // ボタンのイベントを定義する
+    func setEvent() {
+        startButton.addTarget(self,
+                              action: #selector(ViewController.startBUttonTouched(_:)),
+                              forControlEvents: UIControlEvents.TouchUpInside)
+
+        resetButton.addTarget(self,
+                              action: #selector(ViewController.resetButtonTouched(_:)),
+                              forControlEvents: UIControlEvents.TouchUpInside)
+        
+        selectButton.addTarget(self,
+                               action: #selector(ViewController.selectButtonTouched(_:)),
+                               forControlEvents: UIControlEvents.TouchUpInside)
+    }
+
+    // ボタンのイベントメソッド
+    func startBUttonTouched(sender: UIButton) {
+        NSLog("START")
+    }
+
+    func resetButtonTouched(sender: UIButton) {
+        NSLog("RESET")
+    }
+
+    func selectButtonTouched(sender: UIButton) {
+        NSLog("SELECT")
+//        let modalView: ModalViewController = self.storyboard?.instantiateViewControllerWithIdentifier("modal") as! ModalViewController
+//        self.presentViewController(modalView, animated: true, completion: nil)
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
